ff49c188c3c32aedfefb9d2878fe88870abc1e96
mimhussainy <mimhussainy@gmail.com>
2026-06-28 15:51:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ app.use(express.json());
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, PATCH, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   if (req.method === 'OPTIONS') return res.sendStatus(200);
   next();
